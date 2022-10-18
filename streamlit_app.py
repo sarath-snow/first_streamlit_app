1c182f9e8ea604b716300333ab9ddb404a71a4d6
sarath-snow <116045959+sarath-snow@users.noreply.github.com>
2022-10-17 20:37:20 -0500

    Update streamlit_app.py
    added emojis and new bf item

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Idly and Dosa')
-streamlit.text('Vada sambar')
-streamlit.text('Boiled Eggs')
-streamlit.text('Biscuit and Gravy')
+streamlit.header(' Breakfast Menu')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Idly and Dosa')
+streamlit.text('🥑 Vada sambar')
+streamlit.text('🐔 Boiled Eggs')
+streamlit.text('🍞 Biscuit and Gravy')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
