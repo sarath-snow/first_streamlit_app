4c3bd2fc9626b1186711ed3b89453dd2ae54fad5
sarath-snow <116045959+sarath-snow@users.noreply.github.com>
2022-10-17 20:34:27 -0500

    Update streamlit_app.py
    updated Breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.menu('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Idly and Dosa')
+streamlit.text('Vada sambar')
+streamlit.text('Boiled Eggs')
+streamlit.text('Biscuit and Gravy')
